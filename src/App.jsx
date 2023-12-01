5fbe5ae8a9c6fc7670aff6a826cf4b8cc843add7
cherrydub <cherrydub@gmail.com>
2023-12-01 13:19:59 +0000

    using redirect using orderId after its passed from the API response

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import "./App.css";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import { createOrder } from "./services/apiRestaurant";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
+        // errorElement: <Error />,
       },
       {
         path: "/order/:orderId",
