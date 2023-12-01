f4e96071bb8e51f4f3a8da1a2d5d0040211d349b
cherrydub <cherrydub@gmail.com>
2023-12-01 13:47:02 +0000

    fetch data with loaders, and write data using actions, form submitting is better with react-router-dom, no extra state needed, old html way

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -35,6 +35,10 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
 
   return (
     <div>
@@ -52,6 +56,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +79,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -94,6 +101,12 @@ export async function action({ request }) {
 
   console.log(order);
 
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please give us your correct number";
+  if (Object.keys(errors).length > 0) return errors;
+
+  //if everything ok, create new order and re-direct
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
